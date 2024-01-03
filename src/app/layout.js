9dc65d618a569f87a1b7af87ba5575c5ff1aad56
abhay39 <abhayguptaak39@gmail.com>
2024-01-03 19:25:46 +0545

    done

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import { Inter, Poppins } from 'next/font/google'
 import './globals.css'
 import { MyAuthProvider } from '@/hooks'
 import { Toaster } from "react-hot-toast";
-import { cookies } from 'next/headers'
+import Head from 'next/head';
 
 const poppins = Poppins({ subsets: ['latin'],weight:['400','500','600','700'] })
 
@@ -14,6 +14,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" type="image/png" sizes="48x48" href="/icon.png" />
+      </Head>
       <body className={poppins.className}>
         <Toaster position="top-center " />
         <MyAuthProvider>
